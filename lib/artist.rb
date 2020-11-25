bd9fbb3925b422018b709acb383be83feabe88ee
Hunter <kolasa.hunter@aol.com>
2020-11-25 02:45:42 -0500

    commit tree 0b94fe8285f2e378ada340cb607cb99ef1848b15

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,27 @@
+class Artist
+    attr_reader :name, :songs
+    @@all = []
+    def initialize(name)
+        @name = name
+        @songs = songs
+        save
+    end
+    def save
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        
+    end
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+    def genres
+        songs.map {|song| song.genre}
+    end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
+    attr_accessor :name, :doctor
+    @@all = []
+    def initialize(name)
+        @name = name
+        save
+    end
+    def save
+        if @@all.none? {|existing_patient| existing_patient == name}
+            @@all << self
+        end
+    end
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|appt| appt.patient == self}
+    end
+    def doctors
+        appointments.map {|appt| appt.doctor}
+    end
+    def self.all
+        @@all
+    end
+
+
+end
 
